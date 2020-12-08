0d5a927064a46920b87b9ef3641515d1f54d4c02
David Sternlicht <d1618033@gmail.com>
2020-12-08 16:07:42 +0200

    Refactor

--- aoc/day8.py
+++ aoc/day8.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import List
 
@@ -38,34 +38,53 @@ def parse_program(data: List[str]) -> List[Instruction]:
     return [parse_line(line) for line in data if line.strip()]
 
 
-def run_program(program: List[Instruction]) -> ProgramResult:
-    value = 0
-    current_instruction = 0
+@dataclass
+class ProgramState:
+    value: int = field(default=0)
+    current_instruction_index: int = field(default=0)
+
+    def next(self):
+        self.current_instruction_index += 1
+
+
+class ProgramVisitor:
+    @staticmethod
+    def visit_no_operation(program_state: ProgramState, **_):
+        program_state.next()
+
+    @staticmethod
+    def visit_accumulate(program_state: ProgramState, arg: int):
+        program_state.value += arg
+        program_state.next()
+
+    @staticmethod
+    def visit_jump(program_state: ProgramState, arg: int):
+        program_state.current_instruction_index += arg
+
+
+def run_program(program: List[Instruction], visitor: ProgramVisitor) -> ProgramResult:
+    state = ProgramState()
     seen_instructions = set()
-    while current_instruction < len(program):
-        if current_instruction in seen_instructions:
-            logger.debug(f"{current_instruction} already seen")
-            return ProgramResult(ProgramStatus.infinite_loop, value)
-        seen_instructions.add(current_instruction)
-        instruction = program[current_instruction]
-        if instruction.operation == Operation.no_operation:
-            current_instruction += 1
-            continue
-        if instruction.operation == Operation.accumulate:
-            value += instruction.arg
-            current_instruction += 1
-        elif instruction.operation == Operation.jump:
-            current_instruction += instruction.arg
-    return ProgramResult(ProgramStatus.exit, value)
+    while state.current_instruction_index < len(program):
+        if state.current_instruction_index in seen_instructions:
+            logger.debug(f"{state.current_instruction_index} already seen")
+            return ProgramResult(ProgramStatus.infinite_loop, state.value)
+        seen_instructions.add(state.current_instruction_index)
+        instruction = program[state.current_instruction_index]
+        getattr(visitor, f"visit_{instruction.operation.name}")(
+            program_state=state, arg=instruction.arg
+        )
+    return ProgramResult(ProgramStatus.exit, state.value)
 
 
 def part1() -> int:
     program = parse_program(load_input())
-    return run_program(program).value
+    return run_program(program, ProgramVisitor()).value
 
 
 def part2() -> int:
     program = parse_program(load_input())
+    visitor = ProgramVisitor()
     to_replace = {
         Operation.no_operation: Operation.jump,
         Operation.jump: Operation.no_operation,
@@ -78,7 +97,7 @@ def part2() -> int:
                 f"Trying to replace {i}:{instruction.operation}"
                 f" with {new_program[i].operation}"
             )
-            result = run_program(new_program)
+            result = run_program(new_program, visitor)
             if result.status == ProgramStatus.exit:
                 return result.value
     raise AssertionError("One replacement should have fixed it")
