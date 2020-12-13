2ad23e0d4e14a87493c290c8b868045a6f5cda36
David S <d1618033@gmail.com>
2020-12-13 23:14:00 +0200

    Use sympy's crt

--- aoc/day13.py
+++ aoc/day13.py
@@ -1,4 +1,4 @@
-import math
+from sympy.ntheory.modular import crt
 
 from aoc.utils import load_input
 
@@ -15,37 +15,14 @@ def part1():
     return bus * minutes_to_wait(bus)
 
 
-def chinese_remainder(n, a):
-    sum_ = 0
-    prod = math.prod(n)
-    for n_i, a_i in zip(n, a):
-        p = prod // n_i
-        sum_ += a_i * mul_inv(p, n_i) * p
-    return sum_ % prod
-
-
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
-
-
 def part2():
     buses = [
         (int(bus), i) for i, bus in enumerate(load_input()[1].split(",")) if bus != "x"
     ]
-    return chinese_remainder(
-        n=[bus[0] for bus in buses],
-        a=[bus[0] - bus[1] for bus in buses],
-    )
+    return crt(
+        [bus[0] for bus in buses],
+        [bus[0] - bus[1] for bus in buses],
+    )[0]
 
 
 def main():
