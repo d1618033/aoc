83cc17dfa65c9a9853642e72fb764c1655cf56c3
David Sternlicht <d1618033@gmail.com>
2020-12-13 11:05:54 +0200

    Add very initial commit of day13

---
+++ aoc/day13.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import Optional, List, Set, Tuple
+
+from aoc.utils import load_input, load_ints
+
+
+def part1():
+    data = load_input()
+    arrival = int(data[0])
+    buses = [int(bus) for bus in data[1].split(",") if bus != "x"]
+
+    def minutes_to_wait(bus):
+        return bus - arrival % bus
+
+    bus = min(buses, key=minutes_to_wait)
+    return bus * minutes_to_wait(bus)
+
+
+from functools import reduce
+
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
+
+
+def part2():
+    buses = [(int(bus), i) for i, bus in enumerate(load_input()[1].split(",")) if bus != "x"]
+    return chinese_remainder(
+        n=[bus[0] for bus in buses],
+        a=[bus[0] - bus[1] for bus in buses],
+    )
+
+
+def main():
+    print("part1", part1())
+    print("part2", part2())
+
+
+if __name__ == "__main__":
+    main()
