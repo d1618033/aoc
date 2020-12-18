4b6ae7e53ae757648e77fd89695984a2171f5d69
David Sternlicht <d1618033@gmail.com>
2020-12-18 17:10:49 +0200

    Initial commit of day18

---
+++ aoc/day18.py
@@ -0,0 +1,116 @@
+import operator
+
+from arpeggio import (
+    EOF,
+    OneOrMore,
+    Optional,
+    ParserPython,
+    PTNodeVisitor,
+    RegExMatch,
+    ZeroOrMore,
+    visit_parse_tree,
+)
+
+from aoc.utils import load_input
+
+
+def calc_part_1():
+    def expression():
+        return factor, ZeroOrMore(["+", "-", "*", "/"], factor)
+
+    def factor():
+        return Optional(["+", "-"]), [number, ("(", expression, ")")]
+
+    def number():
+        return RegExMatch(r"\d*\.\d*|\d+")
+
+    return OneOrMore(expression), EOF
+
+
+def calc_part_2():
+    def number():
+        return RegExMatch(r"\d*\.\d*|\d+")
+
+    def factor():
+        return Optional(["+", "-"]), [number, ("(", expression, ")")]
+
+    def term():
+        return factor, ZeroOrMore(["+", "-"], factor)
+
+    def expression():
+        return term, ZeroOrMore(["*", "/"], term)
+
+    return OneOrMore(expression), EOF
+
+
+class CalcVisitorPart1(PTNodeVisitor):
+    def visit_number(self, node, _):
+        """
+        Converts node value to float.
+        """
+        if self.debug:
+            print("Converting {}.".format(node.value))
+        return float(node.value)
+
+    def visit_factor(self, _, children):
+        """
+        Applies a sign to the expression or number.
+        """
+        if self.debug:
+            print("Factor {}".format(children))
+        if len(children) == 1:
+            return children[0]
+        sign = -1 if children[0] == "-" else 1
+        return sign * children[-1]
+
+    def visit_expression(self, _, children):
+        """
+        Adds or subtracts terms.
+        Term nodes will be already evaluated.
+        """
+        if self.debug:
+            print("Expression {}".format(children))
+        expr = children[0]
+        operators = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": operator.truediv,
+        }
+        for i in range(2, len(children), 2):
+            expr = operators[children[i - 1]](expr, children[i])
+        if self.debug:
+            print("Expression = {}".format(expr))
+        return expr
+
+
+class CalcVisitorPart2(CalcVisitorPart1):
+    def visit_term(self, node, children):
+        return super().visit_expression(node, children)
+
+
+def evaluate_part_1(line):
+    parser = ParserPython(calc_part_1)
+    return visit_parse_tree(parser.parse(line), CalcVisitorPart1())
+
+
+def evaluate_part_2(line):
+    parser = ParserPython(calc_part_2)
+    return visit_parse_tree(parser.parse(line), CalcVisitorPart2())
+
+
+def part1():
+    return sum(evaluate_part_1(line) for line in load_input())
+
+
+def part2():
+    return sum(evaluate_part_2(line) for line in load_input())
+
+
+def main():
+    print("part1", part1())
+    print("part2", part2())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day18.py
@@ -0,0 +1,17 @@
+import pytest
+
+from aoc.day18 import evaluate_part_1, evaluate_part_2
+
+
+@pytest.mark.parametrize(
+    "line,expected_part_1,expected_part_2",
+    [
+        ("2 * 3 + (4 * 5)", 26, 46),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437, 1445),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240, 669060),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632, 23340),
+    ],
+)
+def test_evaluate(line, expected_part_1, expected_part_2):
+    assert evaluate_part_1(line) == expected_part_1
+    assert evaluate_part_2(line) == expected_part_2
