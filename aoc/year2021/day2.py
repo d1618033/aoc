4c55491ea3457b6018f8340a433d982a7f77e6a2
David Sternlicht <d1618033@gmail.com>
2021-12-02 09:21:18 +0200

    Refactor

--- aoc/year2021/day2.py
+++ aoc/year2021/day2.py
@@ -1,6 +1,24 @@
 from dataclasses import dataclass, field
 
+from pydantic import BaseModel
+
 from aoc.utils import load_input
+
+
+class Command(BaseModel):
+    op: str
+    arg: int
+
+
+def parse(data):
+    commands = []
+    for row in data:
+        match row.split():
+            case (op, arg):
+                commands.append(Command(op=op, arg=arg))
+            case _:
+                raise ValueError(f"Can't parse line: {row}")
+    return commands
 
 
 @dataclass
@@ -15,45 +33,35 @@ class StatePart2(State):
 
 
 def part1():
-    data = load_input()
+    data = parse(load_input())
     state = State()
-    for line in data:
-        match line.split():
-            case (op, arg):
-                arg = int(arg)
-                match op:
-                    case "forward":
-                        state.position += arg
-                    case "up":
-                        state.depth -= arg
-                    case "down":
-                        state.depth += arg
-                    case _:
-                        raise ValueError(f"Unknown op {op}")
+    for cmd in data:
+        match cmd:
+            case Command(op="forward", arg=arg):
+                state.position += arg
+            case Command(op="up", arg=arg):
+                state.depth -= arg
+            case Command(op="down", arg=arg):
+                state.depth += arg
             case _:
-                raise ValueError(f"Can't parse line: {line}")
+                raise ValueError(f"Unknown op {op}")
     return state.position * state.depth
 
 
 def part2():
-    data = load_input()
+    data = parse(load_input())
     state = StatePart2()
-    for line in data:
-        match line.split():
-            case (op, arg):
-                arg = int(arg)
-                match op:
-                    case "forward":
-                        state.position += arg
-                        state.depth += arg * state.aim
-                    case "up":
-                        state.aim -= arg
-                    case "down":
-                        state.aim += arg
-                    case _:
-                        raise ValueError(f"Unknown op {op}")
+    for cmd in data:
+        match cmd:
+            case Command(op="forward", arg=arg):
+                state.position += arg
+                state.depth += state.aim * arg
+            case Command(op="up", arg=arg):
+                state.aim -= arg
+            case Command(op="down", arg=arg):
+                state.aim += arg
             case _:
-                raise ValueError(f"Can't parse line: {line}")
+                raise ValueError(f"Unknown op {op}")
     return state.position * state.depth
 
 
