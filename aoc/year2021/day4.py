66410aa0ac6501f59f97a2cae750b40f7027196c
David Sternlicht <d1618033@gmail.com>
2021-12-04 16:05:29 +0200

    Initial commit of day4

---
+++ aoc/year2021/day4.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass
+from typing import List
+
+from aoc.utils import load_input
+
+
+@dataclass
+class Tile:
+    number: int
+    marked: bool = False
+
+    def __str__(self):
+        if self.marked:
+            return f"*{self.number}*"
+        else:
+            return str(self.number)
+
+
+@dataclass
+class Board:
+    tiles: List[List[Tile]]
+
+    def __str__(self):
+        output = []
+        for row in self.tiles:
+            output.append(" ".join(str(tile) for tile in row))
+        output.append("= " * 10)
+        return "\n".join(output)
+
+    def get_column(self, i):
+        return [row[i] for row in self.tiles]
+
+    def get_row(self, i):
+        return self.tiles[i]
+
+    @property
+    def columns(self):
+        for i in range(len(self.tiles[0])):
+            yield self.get_column(i)
+
+    @property
+    def rows(self):
+        for i in range(len(self.tiles)):
+            yield self.get_row(i)
+
+    @property
+    def won(self):
+        for row in self.rows:
+            if all(tile.marked for tile in row):
+                return True
+        for col in self.columns:
+            if all(tile.marked for tile in col):
+                return True
+        return False
+
+    def mark(self, number):
+        for row in self.rows:
+            for tile in row:
+                if tile.number == number:
+                    tile.marked = True
+                    break
+
+    @property
+    def tiles_flat(self):
+        for row in self.rows:
+            for tile in row:
+                yield tile
+
+    @property
+    def sum_unmarked(self):
+        return sum(tile.number for tile in self.tiles_flat if not tile.marked)
+
+
+def get_parsed_data():
+    data = load_input(skip_empty=False)
+    numbers = list(map(int, data[0].split(",")))
+    boards = []
+    current = Board(tiles=[])
+    boards.append(current)
+    for line in data[2:-1]:
+        if line.strip() == "":
+            current = Board(tiles=[])
+            boards.append(current)
+        else:
+            current.tiles.append(
+                list(map(lambda x: Tile(number=int(x)), line.strip().split()))
+            )
+    return boards, numbers
+
+
+def part1():
+    boards, numbers = get_parsed_data()
+    for number in numbers:
+        for board in boards:
+            board.mark(number)
+            if board.won:
+                return board.sum_unmarked * number
+
+
+def part2():
+    boards, numbers = get_parsed_data()
+    winning_boards = []
+    for number in numbers:
+        for board in boards:
+            if board in winning_boards:
+                continue
+            board.mark(number)
+            if board.won:
+                winning_boards.append(board)
+            if len(winning_boards) == len(boards):
+                return board.sum_unmarked * number
+
+
+def main():
+    print("part1", part1())
+    print("part2", part2())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/year2021/test_day4.py
@@ -0,0 +1,9 @@
+from aoc.year2021.day4 import part1, part2
+
+
+def test_part1():
+    assert part1() == 4512
+
+
+def test_part2():
+    assert part2() == 1924
