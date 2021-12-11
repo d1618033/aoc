08b7022788e21e4332031cdb60bb6184a9c52619
David Sternlicht <d1618033@gmail.com>
2021-12-11 09:40:08 +0200

    Initial commit of day11

--- aoc/year2021/day11.py
+++ aoc/year2021/day11.py
@@ -1,12 +1,43 @@
-from aoc.utils import load_board_of_ints
+from aoc.utils import get_neighbors, load_board_of_ints
+
+
+def step(board):
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
+            board[i][j] += 1
+    flashes_to_do = set()
+    for i, row in enumerate(board):
+        for j, cell in enumerate(row):
+            if board[i][j] > 9:
+                flashes_to_do.add((i, j))
+    flashed = set()
+    while flashes_to_do:
+        (i, j) = flashes_to_do.pop()
+        flashed.add((i, j))
+        for (ni, nj) in get_neighbors(i, j, len(board), len(row), diagonal=True):
+            board[ni][nj] += 1
+            if board[ni][nj] > 9 and (ni, nj) not in flashed:
+                flashes_to_do.add((ni, nj))
+    for (i, j) in flashed:
+        board[i][j] = 0
+    return flashed
 
 
 def part1():
     board = load_board_of_ints()
+    total_flashed = 0
+    for _ in range(100):
+        flashed = step(board)
+        total_flashed += len(flashed)
+    return total_flashed
 
 
 def part2():
-    ...
+    board = load_board_of_ints()
+    for i in range(1000):
+        flashed = step(board)
+        if len(flashed) == len(board) * len(board[0]):
+            return i + 1
 
 
 def main():
--- tests/year2021/test_day11.py
+++ tests/year2021/test_day11.py
@@ -2,8 +2,8 @@ from aoc.year2021.day11 import part1, part2
 
 
 def test_part1():
-    assert part1() is None
+    assert part1() == 1656
 
 
 def test_part2():
-    assert part2() is None
+    assert part2() == 195
