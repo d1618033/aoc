a79103731162c7af111fa366697b1b17856a6995
David Sternlicht <d1618033@gmail.com>
2021-12-17 09:06:04 +0200

    Initial commit of day17

---
+++ aoc/year2021/day17.py
@@ -0,0 +1,27 @@
+import re
+import functools
+import itertools
+import collections
+
+from dataclasses import dataclass
+from typing import Optional, List, Set, Tuple
+from pydantic import BaseModel
+
+from aoc.utils import load_input, print_
+
+
+def part1():
+    ...
+
+
+def part2():
+    ...
+
+
+def main():
+    print("part1", part1())
+    print("part2", part2())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/year2021/test_day17.py
@@ -0,0 +1,10 @@
+import pytest
+from aoc.year2021.day17 import part1, part2
+
+
+def test_part1():
+    assert part1() is None
+
+
+def test_part2():
+    assert part2() is None
